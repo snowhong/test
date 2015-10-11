24f4f3d56321c6dcb8b64769afeea48cca2ceecf
snowhong <snowhong@foxmail.com>
2015-10-11 02:09:49 +0100

    nothing

--- auto_left.py
+++ auto_left.py
@@ -11,10 +11,14 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 #from sonar import SonarArray 
 INIT = 1
-SAFE_FORWARD = 0.3
-SAFE_LEFT = 0.3
-SAFE_RIGHT= 0.3
-LASER_RANGE = 360
+SAFE_FORWARD = 0.5
+SAFE_LEFT = 1
+SAFE_RIGHT = 1
+LASER_RANGE = 500
+RIGHT_LASER_RANGE1 = 0
+RIGHT_LASER_RANGE2 = LASER_RANGE/3
+LEFT_LASER_RANGE1 = LASER_RANGE - LASER_RANGE/3
+LEFT_LASER_RANGE2 = LASER_RANGE - 1
 corner = 0
 init_min = 999
 PRE_LEFT_LASER = 0
@@ -22,24 +26,27 @@ LEFT_LIMIT = 1
 
 def callback(sensor_data):
 	base_data = Twist()
-	forward_data = sensor_data.ranges[LASER_RANGE/4:3*LASER_RANGE/4]
-	forward_data = filter(lambda v: v==v, forward_data)
+	pre_forward_data= sensor_data.ranges[RIGHT_LASER_RANGE2:LEFT_LASER_RANGE1]
+	forward_data = filter(lambda v: v==v, pre_forward_data)
 
 #forward_data = forward_data[math.isnan(forward_data)]
-	forward_dis = numpy.min(sensor_data.ranges[LASER_RANGE/4:3*LASER_RANGE/4])
+	forward_dis = numpy.min(forward_data)
 #right_dis = numpy.min(sensor_data.ranges[0:LASER_RANGE/3])
 #left_dis = numpy.min(sensor_data.ranges[LASER_RANGE-LASER_RANGE/3:LASER_RANGE])
+	##FIXME:Only select one value test the rubustness.
 	right_dis = sensor_data.ranges[0]
-	left_dis = sensor_data.ranges[LASER_RANGE-1]
+	left_dis = sensor_data.ranges[RIGHT_LASER_RANGE2]
+
 	global corner
 	global init_min 
 	global PRE_LEFT_LASER
+	global INIT
 
 	if INIT == 1:
 		#Get The Closet Point and Go There!
+		##TODO:Conditions may occur when robot can't get a value.
 		if(init_min > min(sensor_data.ranges)):
 			init_min = min(sensor_data.ranges)
-		global INIT
 		print(len(sensor_data.ranges))
 		print time.strftime("%H:%M:%S----"),forward_dis
 		print 'init_min:',init_min
@@ -74,7 +81,8 @@ def callback(sensor_data):
 		if corner == 2:
 			if left_dis <= SAFE_LEFT and right_dis <= SAFE_RIGHT:
 				print "avoid corner stuck!"
-				if right>= left_dis:
+				##FIXME: Here may let robot get stuck at corner, turn left and then turn right.
+				if right_dis >= left_dis:
 					base_data.angular.z = -0.2
 				else:
 					base_data.angular.z = 0.2
@@ -113,6 +121,6 @@ def callback(sensor_data):
 
 if __name__ == '__main__':
 	rospy.init_node('wall_mover')
-	rospy.Subscriber('/p3dx/laser/scan', LaserScan, callback)
+	rospy.Subscriber('/base_scan', LaserScan, callback)
 	pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 	rospy.spin()
